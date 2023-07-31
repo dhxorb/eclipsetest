ee3b0a6f0c5e48216ab0a9c1fd53bdbb5ff75b47
dhxorb <osaeroi@naver.com>
2023-07-31 17:20:33 +0900

    second commit

--- src/eclipsetest/testeclipse.java
+++ src/eclipsetest/testeclipse.java
@@ -3,3 +3,8 @@ package eclipsetest;
 public class testeclipse {
 
 }
+//eclipse 경험상 project 열어서 show in browser에서 git bash연다음에
+//git bash에서 (vscode에서  터미널 열고 명령어 입력해서 깃허브에 저장하듯이)똑같은 명령어 입력하고 깃허브에 저장하면 된다.
+//git add .
+//git commit -m "first commit"
+//git push origin master
